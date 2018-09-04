0f6075f913b4574f1cc2a88a927f96e91b3ce97e
gilmar <jiraspiom@yahoo.com.br>
2018-09-04 10:21:50 -0300

    Adicionado o layout que ira reproduzir o video, usando o componente videoview

--- app/src/main/java/br/com/example/alalgi/executandovideo/PlayerActivity.java
+++ app/src/main/java/br/com/example/alalgi/executandovideo/PlayerActivity.java
@@ -2,17 +2,12 @@ package br.com.example.alalgi.executandovideo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class PlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_player);
     }
-    public void abrirVideo(View view){
-
-    }
-
 }

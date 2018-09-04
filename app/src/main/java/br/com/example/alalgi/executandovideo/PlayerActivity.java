74adac503b3fdd97242d228c2336cb381c7ca14e
gilmar <jiraspiom@yahoo.com.br>
2018-09-04 10:57:24 -0300

    PROJETO FINALIZADO

--- app/src/main/java/br/com/example/alalgi/executandovideo/PlayerActivity.java
+++ app/src/main/java/br/com/example/alalgi/executandovideo/PlayerActivity.java
@@ -2,6 +2,7 @@ package br.com.example.alalgi.executandovideo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -14,6 +15,15 @@ public class PlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_player);
 
         videoView = findViewById(R.id.videoView);
+
+        //esconder status bar e barra de navegacao
+        View decorView = getWindow().getDecorView();
+        //int uiOpcoes = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        int uiOpcoes = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOpcoes);
+
+        //esconder actionbar
+        getSupportActionBar().hide();
 
         //controle do video
         videoView.setMediaController(new MediaController(this));
